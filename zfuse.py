1c553ec1e01d813a8cf7604cd7943e05fbaa03d3
kassoulet <kassoulet@gmail.com>
2012-05-13 18:22:20 +0200

    update

--- zfuse.py
+++ zfuse.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # 
-# zfuse, simple transparent decompression for fuse
+# gzfuse - simple, transparent gz decompression for fuse
 # 
 # 
-# Copyright (c) 2011 Gautier Portet <kassoulet+gmail>
+# Copyright (c) 2012 Gautier Portet <kassoulet+gmail>
 # 
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -18,8 +18,8 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 from errno import EACCES
-from os.path import realpath
-from sys import argv #, exit
+from os.path import realpath, join
+from sys import argv, stdout
 from threading import Lock
 import gzip
 import os
@@ -27,66 +27,72 @@ import os
 # http://code.google.com/p/fusepy
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
-base = {}
+#base = {}
 
-def convert_filename(filename):
+def log(*args):
+    return
+    print ' '.join(str(s) for s in args)
+    stdout.flush()
+
+def error(*args):
+    print ' '.join(str(s) for s in args)
+    stdout.flush()
+    raise SystemExit
+
+def convert_filename(filename, path):
     if filename.endswith('.gz'):
         original = filename
         filename = filename[:-len('.gz')]
-        base['/' + filename] = '/' + original
+        #base[join(path, filename)] = join(path, original)
     return filename
-    
 
-class Loopback(Operations, LoggingMixIn):    
+counter = 0
+really = 0
 
-    chmod = os.chmod
-    chown = os.chown
+class Loopback(Operations):
+
     fsync = None
     listxattr = 1
-    mkdir = os.mkdir
     mknod = None
     getxattr = None
     readlink = 3
     rmdir = None
     statfs = None
     symlink = None
-    unlink = os.unlink
     utimens = os.utime
 
     def __init__(self, root):
         self.root = realpath(root)
-        self.rwlock = Lock()
+        #self.rwlock = Lock()
+        self.fileslock = Lock() # TODO: useful ?
         self.files = {}
+        self.content = {}
     
     def __call__(self, op, path, *args):
-        if path in base:
-            path = base[path]
+        #log('call: %s %s %s' % (op, path, args))
+        #if path in base:
+        #    print ' ', path, '->' ,base[path]
+        #    path = base[path]
+        if not os.path.exists(self.root + path):
+            if os.path.exists(self.root + path + '.gz'):
+                path += '.gz'
         return super(Loopback, self).__call__(op, self.root + path, *args)
     
     def access(self, path, mode):
-        print 'access:', path, mode
+        log('access:', path, mode)
         if not os.access(path, mode):
             raise FuseOSError(EACCES)
     
-    def create(self, path, mode):
-        self.files[path+'.gz'] = gzip.open(path+'.gz', 'wb')
-        path = path[len(self.root)+1:]
-        convert_filename(path+'.gz')
-        return 1
-    
     def flush(self, path, fh):
-        if path not in self.files:
-            print 'flush error:', path, 'not in', self.files 
-            return
-        return self.files[path].flush()
+        return self.files[fh].flush()
 
     def getattr(self, path, fh=None):
-        print 'getattr:', path, fh
+        log('getattr:', path, fh)
         st = os.lstat(path)
         d = dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
             'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-        print d
         if path.endswith('.gz'):
+            # read file size from gz header
             with open(path, 'rb') as f:
                 size = 0
                 try:
@@ -97,62 +103,56 @@ class Loopback(Operations, LoggingMixIn):
                 except IOError:
                     pass
                 d['st_size'] = size
+        d['st_mode'] &= 0xffffff6d # remove write attr
         return d
-    
-    def link(self, target, source):
-        print 'link:', source, '->', target
-        raise NotImplementedError
-        return os.link(source, target)
     
     def open(self, path, flags):
         mode = 'r'
         if flags & os.O_WRONLY:
-            mode = 'w'
+            error('Write mode is not supported!')
+            return 0
+            #mode = 'w'
         if flags & os.O_APPEND:
-            mode = 'a'
+            error('Append mode is not supported!')
+            return 0
+            #raise NotImplementedError
         if path.endswith('.gz'):
-            self.files[path] = gzip.open(path, mode+'b3')
+            f = gzip.open(path, mode+'b3')
         else:
-            self.files[path] = open(path, mode+'b')
-        print 'open:', path, mode
-        return 1        
+            f = open(path, mode+'b')
+        
+        with self.fileslock:
+            global counter
+            counter += 1
+            self.files[counter] = f
+            #if not path in self.content:
+            #    global really
+            #    really += 1
+            #    self.content[path] = f.read()
+            fh = counter
+        log('open:', path, mode, fh)
+        return fh     
         
     def read(self, path, size, offset, fh):
-        print 'read: %s %s %s' % (path, offset, size)
-        if path not in self.files:
-            print 'read error:', path, 'not in', self.files
-        with self.rwlock:
-            self.files[path].seek(offset, 0)
-            return self.files[path].read(size)
+        log('read: %s %s %s %s' % (path, offset, size, fh))
+        #with self.rwlock:
+        #return self.content[path][offset:offset+size]
+        self.files[fh].seek(offset)
+        return self.files[fh].read(size)
     
     def readdir(self, path, fh):
-        print 'readdir:', path
-        return ['.', '..'] + [convert_filename(f) for f in os.listdir(path)]
+        relpath = path[len(self.root):]
+        log('readdir:', path)
+        l = ['.', '..'] + [convert_filename(f, relpath) for f in os.listdir(path)]
+        return l
 
     def release(self, path, fh):
-        if path not in self.files:
-            print 'release error:', path, 'not in', self.files
-            return
-        r = self.files[path].close()
-        del self.files[path]
+        log('close: %s %s' % (path, fh))
+        r = self.files[fh].close()
+        with self.fileslock:
+            del self.files[fh]
         return r 
-        
-    def rename(self, old, new):
-        print 'rename:', old, '->', self.root + new
-        return os.rename(old, self.root + new)
-
-    def truncate(self, path, length, fh=None):
-        if length > 0:
-            print 'truncate:', path, length
-            raise NotImplementedError
-        open(path, 'wb').close()
-    
-    def write(self, path, data, offset, fh):
-        print 'write: %s %s %s' % (path, offset, len(data))
-        with self.rwlock:
-            self.files[path].seek(offset, 0)
-            return self.files[path].write(data)
-    
+            
 
 if __name__ == "__main__":
     if len(argv) != 3:
@@ -161,5 +161,5 @@ if __name__ == "__main__":
     fuse = FUSE(Loopback(argv[1]), argv[2], 
                 foreground=True, debug=False, nothreads=True)
     
-    
+print 'gzfuse exited. %d files read, %d files really read' % (counter, really)
     
